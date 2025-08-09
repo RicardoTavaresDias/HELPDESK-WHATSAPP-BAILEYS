6b837981f540337b4c23e4c18363fe887ca33fda
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-08-09 14:24:58 -0300

    version: 1.0.9

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -1,10 +1,6 @@
 import { AI, model } from "@/config/google.config";
-import client from "@/config/postgres.config";
 
 async function main() {
-  await client.connect();
-  const res = await client.query('SELECT id, name, email, role FROM "user"');
-
   const response = await AI.models.generateContent({
     model,
     contents: "Quem é você? o que você poderá me ajudar?",
