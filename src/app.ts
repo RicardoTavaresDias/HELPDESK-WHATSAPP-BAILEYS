f35285027cff588fd86ea2155cef4b058d88d886
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-08-08 21:20:25 -0300

    version: 1.0.4

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,17 @@ import { errorHandling } from "./middlewares/errorHandling"
 const app = express()
 app.use(express.json())
 app.use(router)
-app.use(cors())
+
+const allowedOrigins = [
+  'http://localhost:3333/',
+  'https://helpdesk-react-iota.vercel.app',
+  'http://localhost:5173'
+]
+
+app.use(cors({
+  origin: allowedOrigins
+}))
+
 app.use(errorHandling)
 
 export { app }
